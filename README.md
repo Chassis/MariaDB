af91a4bcbb615cb1631aa7bd4ced84fda6ba7ac8
Bronson Quick <bronson@bronsonquick.com.au>
2017-09-02 14:54:50 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# A MariaDB extension for Chassis
+The MariaDB extension automatically sets up your Chassis instance to be able to use MariaDB.
+
+## Activation
+Ensure you have a Chassis instance set up locally already.
+
+```
+# In your Chassis dir:
+git clone --recursive https://github.com/Chassis/MariaDB.git extensions/mariadb
+```
+
+Then you'll need to reprovision
+```
+cd ..
+vagrant provision
+```
+
+Alternatively you can add the extension to one of your yaml config files. e.g.
+```
+# Extensions
+#
+# Install a list of extensions automatically
+extensions:
+    - chassis/mariadb
+```
+
+Then you'll need to reprovision
+
+```
+cd ..
+vagrant provision
+```
+
+MariaDB has now been installed inside your Chassis box.
---
+++ modules/mariadb/manifests/init.pp
@@ -0,0 +1,37 @@
+class mariadb (
+	$path   = '/vagrant/extensions/mariadb',
+) {
+
+	apt::source { 'mariadb':
+		location => 'http://mirror.aarnet.edu.au/pub/MariaDB/repo/10.2/ubuntu',
+		release  => $::lsbdistcodename,
+		repos    => 'main',
+		key      => {
+			id     => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+			server => 'hkp://keyserver.ubuntu.com:80',
+		},
+		include => {
+			src   => false,
+			deb   => true,
+		},
+	}
+	class { '::mysql::server':
+		package_name     => 'mariadb-server',
+		package_ensure   => 'latest',
+		service_name     => 'mysql',
+		root_password    => 'password',
+		override_options => {
+			mysqld => {
+				'log-error' => '/var/log/mysql/mariadb.log',
+				'pid-file'  => '/var/run/mysqld/mysqld.pid',
+			},
+			mysqld_safe => {
+				'log-error' => '/var/log/mysql/mariadb.log',
+			},
+		}
+	}
+
+	Apt::Source['mariadb'] ~>
+	Class['apt::update'] ->
+	Class['::mysql::server']
+}
